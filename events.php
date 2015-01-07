90caf1c46f07451f4e5e574724d1034f2c06dfa8
Daniel Bielczyk <daniel@koderskapohotovost.cz>
2015-01-07 18:48:55 +0100

    events

---
+++ events.php
@@ -0,0 +1,26 @@
+<?php require_once '_header.php'; ?>
+
+<main>
+	<div class="inner">
+		<div class="events">
+			<article class="event">
+				<img src="images/event1.jpg" alt="" />
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus urna arcu, finibus facilisis nisi placerat ut. Sed ipsum enim, laoreet vitae diam at, suscipit tincidunt nulla. Donec facilisis volutpat odio, a blandit libero. Nam aliquam sit amet nisi quis lobortis. Cras sit amet sem quam. Proin cursus odio id sem vehicula, sed consequat quam consequat. Nunc ligula tellus, volutpat at leo vel, semper dictum magna.</p>
+			</article>
+			<article class="event">
+				<img src="images/event2.jpg" alt="" />
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus urna arcu, finibus facilisis nisi placerat ut. Sed ipsum enim, laoreet vitae diam at, suscipit tincidunt nulla. Donec facilisis volutpat odio, a blandit libero. Nam aliquam sit amet nisi quis lobortis. Cras sit amet sem quam. Proin cursus odio id sem vehicula, sed consequat quam consequat. Nunc ligula tellus, volutpat at leo vel, semper dictum magna.</p>
+			</article>
+			<article class="event">
+				<img src="images/event3.jpg" alt="" />
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus urna arcu, finibus facilisis nisi placerat ut. Sed ipsum enim, laoreet vitae diam at, suscipit tincidunt nulla. Donec facilisis volutpat odio, a blandit libero. Nam aliquam sit amet nisi quis lobortis. Cras sit amet sem quam. Proin cursus odio id sem vehicula, sed consequat quam consequat. Nunc ligula tellus, volutpat at leo vel, semper dictum magna.</p>
+			</article>
+			<article class="event">
+				<img src="images/event4.jpg" alt="" />
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus urna arcu, finibus facilisis nisi placerat ut. Sed ipsum enim, laoreet vitae diam at, suscipit tincidunt nulla. Donec facilisis volutpat odio, a blandit libero. Nam aliquam sit amet nisi quis lobortis. Cras sit amet sem quam. Proin cursus odio id sem vehicula, sed consequat quam consequat. Nunc ligula tellus, volutpat at leo vel, semper dictum magna.</p>
+			</article>
+		</div>
+	</div>
+</main>
+
+<?php require_once '_footer.php';
