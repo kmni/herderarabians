7718a9477d98a48b096031ad350005b7a2beba54
Daniel Bielczyk <daniel@koderskapohotovost.cz>
2015-01-07 13:56:23 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="cs">
+	<head>
+		<meta charset="utf-8" />
+		<title></title>
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="author" content="" />
+		<meta name="robots" content="index,follow" />
+
+		<link href="./styles/style.css" rel="stylesheet" media="screen" />
+		<link href="./styles/print.css" rel="stylesheet" media="print" />
+		
+		<!--[if lt IE 9]>
+			<script src="./scripts/html5shiv.js"></script>
+		<![endif]-->
+
+		<script src="./scripts/jquery.js"></script>
+		
+		<script src="./kptoolbar/kptoolbar.js"></script>
+		
+		<script src="./scripts/main.js"></script>
+		
+	</head>
+	<body>
+		
+		<header>
+			<nav>
+				<ul>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+				</ul>
+			</nav>
+		</header>
+		
+		<main>
+			
+		</main>
+		
+		<footer>
+			
+		</footer>
+		
+	</body>
+</html>
